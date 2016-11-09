d1589d8cb1ced5cc73a658f9382dcc60fc5efe51
cyper <345343747@qq.com>
2016-11-09 22:08:53 +0800

    Initial Commit

---
+++ KittenSDK/KittenSDK.h
@@ -0,0 +1,19 @@
+//
+//  KittenSDK.h
+//  KittenSDK
+//
+//  Created by cyper on 09/11/2016.
+//  Copyright © 2016 cyper tech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KittenSDK.
+FOUNDATION_EXPORT double KittenSDKVersionNumber;
+
+//! Project version string for KittenSDK.
+FOUNDATION_EXPORT const unsigned char KittenSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KittenSDK/PublicHeader.h>
+
+
