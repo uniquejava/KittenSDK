864f08e620765b119029ba28b643109fd0e8c559
cyper <345343747@qq.com>
2016-11-09 22:10:49 +0800

    Develop Functionality

---
+++ KittenSDK/KittenSDK.swift
@@ -0,0 +1,15 @@
+//
+//  KittenSDK.swift
+//  KittenSDK
+//
+//  Created by cyper on 09/11/2016.
+//  Copyright © 2016 cyper tech. All rights reserved.
+//
+
+import Foundation
+
+public class KittenSDK {
+    public class func logToConsole(msg: String) {
+        print(msg)
+    }
+}
